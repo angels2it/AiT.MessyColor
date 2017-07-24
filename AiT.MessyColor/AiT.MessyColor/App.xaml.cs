90fc1eba4dd80b595748444355484fdf6f1a7aea
cuongpn <cuong.pham@eton.vn>
2017-07-24 16:39:40 +0700

    Add project files.

---
+++ AiT.MessyColor/AiT.MessyColor/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.Unity;
+using AiT.MessyColor.Views;
+using Xamarin.Forms;
+
+namespace AiT.MessyColor
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
---
+++ AiT.MessyColor/AiT.MessyColor/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AiT.MessyColor.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
